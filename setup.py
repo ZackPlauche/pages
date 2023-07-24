3c014fc55dd126daa0e44815718622c94eb36cd0
Zack Plauche <zackplauche@gmail.com>
2023-07-24 05:53:46 +0100

    Simplify reqs & setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 from setuptools import setup, find_packages
 
-def parse_requirements(filename):
-    with open(filename) as f:
-        lines = f.read().splitlines()
-    return [line for line in lines if not line.startswith("-")]
-
-required = parse_requirements('requirements.txt')
+with open('requirements.txt') as file:
+    requirements = file.read().splitlines()
 
 setup(
     name='pages',
-    version='0.1',
+    version='0.1.0',
     url='https://github.com/ZackPlauche/pages',
     author='Zack Plauche',
     author_email='zackknowspython@gmail.com',
     description='A simple library for creating page objects in Python',
     packages=find_packages(),
-    install_requires=required,
+    install_requires=requirements,
 )
